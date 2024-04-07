11418bd53f90080bb6021bd2c96e949f56766142
Chehak <chehaksharma55@gmail.com>
2024-04-06 23:28:54 -0700

    partially upload page done

--- frontend/app/(tabs)/upload.tsx
+++ frontend/app/(tabs)/upload.tsx
@@ -1,12 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-const Page = () => {
+const UploadScreen = () => {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
-    <View>
-      <Text>Picture</Text>
+    <View style={styles.container}>
+      <View style={styles.uploadContainer}>
+        <Text style={styles.title}>Daily Photo Upload</Text>
+        <TouchableOpacity onPress={pickImage} style={styles.uploadButton}>
+          <Text style={styles.uploadButtonText}>Tap to upload photo from camera</Text>
+        </TouchableOpacity>
+        {image && <Image source={{ uri: image }} style={styles.previewImage} />}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Page
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff',
+  },
+  uploadContainer: {
+    marginTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  uploadButton: {
+    backgroundColor: '#e7e7e7',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  uploadButtonText: {
+    fontSize: 16,
+    color: '#4a4a4a',
+  },
+  previewImage: {
+    width: 300,
+    height: 300,
+    borderRadius: 10,
+  },
+});
+
+export default UploadScreen;
