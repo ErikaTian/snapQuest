536088507bd21d5c3710c705cace7521951a5c44
erika_cats <scenery@student.ubc.ca>
2024-04-06 19:45:41 -0700

    debug for setup

--- backend/snapQuest/src/main/java/com/example/snapquest/SnapQuestApplication.java
+++ backend/snapQuest/src/main/java/com/example/snapquest/SnapQuestApplication.java
@@ -8,7 +8,7 @@ public class SnapQuestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnapQuestApplication.class, args);
-		System.out.println("hello");
+		System.out.println("hello, it is me");
 	}
 
 }
