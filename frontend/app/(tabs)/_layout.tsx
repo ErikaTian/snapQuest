c1cdd4fcbf74648ef2f13eb58f31a2444b57aabe
Anica1004 <anicamok@gmail.com>
2024-04-07 10:05:53 -0700

    theme

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { Tabs } from 'expo-router';
 import Colors from '@/constants/Colors';
 import { MaterialCommunityIcons, AntDesign, MaterialIcons, FontAwesome } from '@expo/vector-icons';
-
+import {useState, useEffect} from 'react'; 
+import { EventRegister } from 'react-native-event-listeners';
 const Layout = () => {
+  const[darkMode, setDarkMode] = useState(false)
+
+  useEffect (() => {
+    const listener = EventRegister.addEventListener('ChangeTheme', (data)=>{
+      setDarkMode(data)
+    })
+    return () => {
+      EventRegister.removeAllListeners(listener)
+    }
+  }, [darkMode])
+  
   return (
     <Tabs
       screenOptions={{
